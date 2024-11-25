c199d160ea49fb915ea5b7662723e3c5343cc8fd
Dang <huynhminhdang@gmail.com>
2024-11-25 20:16:44 +0800

    `-w` to watch

--- conf.d/dark.fish
+++ conf.d/dark.fish
@@ -1,15 +1,22 @@
-function dark -a action
+function dark
+    argparse -i w/watch -- $argv
+    set -l action $argv[1]
+
     switch $action
         case restart
             pkill -x dark >/dev/null 2>&1
     end
 
-    nohup lockf -t 0 $TMPDIR/dark.lock fish -P -c '
-        command dark | while read -l mode
-            emit apple_interface_theme_changed $mode
-        end
-    ' >/dev/null 2>&1 &
-    disown
+    if set -q _flag_watch
+        nohup lockf -t 0 $TMPDIR/dark.lock fish -P -c '
+            command dark -w | while read -l mode
+                emit apple_interface_theme_changed $mode
+            end
+        ' >/dev/null 2>&1 &
+        disown
+    else
+        command dark
+    end
 end
 
-dark
+dark -w
